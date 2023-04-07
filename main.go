e637ef82c36cf1acc949255258ccd1febe7e2d5a
Nguyen Quang Phuc <nguyenquangphuc.vn.hue@gmail.com>
2023-04-07 21:07:51 +0700

    update CRUD with criteria

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"net/http"
 	"os"
 	"strconv"
-	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -30,6 +29,20 @@ type ToDoItem struct {
 }
 
 func (ToDoItem) TableName() string { return "todo_items" }
+
+type ToDoItemCreation struct {
+	Id    int    `json:"-" gorm:"column:id;"` // return id of todoItem after creating
+	Title string `json:"title" gorm:"column:title;"`
+}
+
+func (ToDoItemCreation) TableName() string { return ToDoItem{}.TableName() }
+
+type ToDoItemUpdate struct {
+	Title  *string `json:"title" gorm:"column:title;"`
+	Status *string `json:"status" gorm:"column:status;"`
+}
+
+func (ToDoItemUpdate) TableName() string { return ToDoItem{}.TableName() }
 
 func main() {
 	dsn := os.Getenv("DB_STRING")
@@ -57,23 +70,24 @@ func main() {
 
 func createItem(db *gorm.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		var dataItem ToDoItem
+		var dataItem ToDoItemCreation
 
 		if err := c.ShouldBind(&dataItem); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
 
-		// preprocess title - trim all spaces
-		dataItem.Title = strings.TrimSpace(dataItem.Title)
-
-		if dataItem.Title == "" {
-			c.JSON(http.StatusBadRequest, gin.H{"error": "title cannot be blank"})
-			return
-		}
-
-		// do not allow "finished" status when creating a new task
-		dataItem.Status = "Doing" // set to default
+		//// preprocess title - trim all spaces
+		//dataItem.Title = strings.TrimSpace(dataItem.Title)
+		//
+		//if dataItem.Title == "" {
+		//	c.JSON(http.StatusBadRequest, gin.H{"error": "title cannot be blank"})
+		//	return
+		//}
+		//
+		//// do not allow "finished" status when creating a new task
+		//dataItem.Status = "Doing" // set to default
+		//
 
 		if err := db.Create(&dataItem).Error; err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
@@ -153,7 +167,7 @@ func editItemById(db *gorm.DB) gin.HandlerFunc {
 			return
 		}
 
-		var dataItem ToDoItem
+		var dataItem ToDoItemUpdate
 
 		if err := c.ShouldBind(&dataItem); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
